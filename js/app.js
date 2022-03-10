c8d8a2a4bd9088d4859a9c9c8ebb000fbb631b67
subrata sarker <tccdcc8@gmail.com>
2022-03-10 22:07:02 +0600

    liked photo tab is fixed

--- js/app.js
+++ js/app.js
@@ -36,7 +36,8 @@ const switchTab = (id) => {
     document.getElementById("posts").style.display = "grid";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
-  } else if (id === "liked") {
+  }
+  else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
@@ -145,6 +146,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById("liked").textContent = "";
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
